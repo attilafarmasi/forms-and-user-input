21190b27c490b28f24756391e0c237f239655852
Attila Farmasi <attila.farmasi@dpdgroup.com>
2023-11-23 16:14:22 +0100

    Validating Input on Every Keystroke via State

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -8,6 +8,7 @@ export default function Login() {
 		password: ''
 	});
 
+	const emailIsInvalid = enteredValues.email !== '' && !enteredValues.email.includes('@');
 
 	function handleSubmit(event) {
 		event.preventDefault();
@@ -40,6 +41,7 @@ export default function Login() {
 						onChange={(event) => handleInputChange('email', event.target.value)}
 						value={enteredValues.email}
 					/>
+					<div className={'control-error'}>{emailIsInvalid && <p>Please enter a valid email address.</p>}</div>
 				</div>
 
 				<div className="control no-margin">
